3184d508fe7fb857afe7487fc64ca17db9423250
Christianzer <christianaude2017@gmail.com>
2020-06-21 16:06:29 +0000

    modif(loading-page)

--- resources/views/pages/loading.blade.php
+++ resources/views/pages/loading.blade.php
@@ -3,9 +3,9 @@
     <title>BIENVENUE CHEZ DOWJIL CAPITAL</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('frontend/css/jquery-loading.css')}}">
-    <link rel="stylesheet" href="{{asset('frontend/css/styleBackground.css')}}">
+    <!-- <link rel="stylesheet" href="{{asset('frontend/css/styleBackground.css')}}">-->
 
     <!--<link href="css/jquery-loading.css" rel="stylesheet">-->
     <!--<script src="//code.jquery.com/jquery-3.1.0.min.js"></script>-->
@@ -15,7 +15,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <script src="{{asset('frontend/js/jquery-loading.js')}}"></script>
 </head>
-<body class="" id="menu">
+<body class="" id="menu" style="background-image: url({{asset('frontend/images/logo.PNG')}});background-repeat: no-repeat;background-position: 50% 40%;background-size: auto;">
 <div class="row col-lg-12" style="height: 100%">
 <!--            <img conta="img-fluid" src="{{asset('frontend/images/bg_2.jpg')}}">-->
     <div class="container-fluid">
